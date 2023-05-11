273459f05158c50259e2e86a8dac124547fc8bec
jhonthe77 <jhonthe7@gmail.com>
2023-05-10 23:51:28 -0500

    js para las skill dinamicas

--- main.js
+++ main.js
@@ -10,38 +10,37 @@ function MostrarOcultarMenu(menuVisble) {
 }
 
 function selecionar() {
-     document.getElementById("nav").classList = "";
-     menuVisble = false;
-    
+  document.getElementById("nav").classList = "";
+  menuVisble = false;
 }
 
 //Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
-    let habilidadesProgreso = [
-      "java",
-      "python",
-      "javascript",
-      "html-css",
-      "express",
-      "mysql",
-      "liderasgo",
-      "tiempo",
-      "html-css",
-      "html-css",
-      "java",
-      "liderasgo",
-    ];
-    let skills = document.getElementById("skills");
-    let distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        for (var i = 0; i < habilidades.length; i++) {
-            habilidades[i].classList.add(habilidadesProgreso[i]);
-        }     
+function efectoHabilidades() {
+  let habilidadesProgreso = [
+    "java",
+    "python",
+    "javascript",
+    "html-css",
+    "express",
+    "mysql",
+    "liderasgo",
+    "liderasgo",
+    "html-css",
+    "html-css",
+    "liderasgo",
+    "liderasgo",
+  ];
+  let skills = document.getElementById("skills");
+  let distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    let habilidades = document.getElementsByClassName("progreso");
+    for (var i = 0; i < habilidades.length; i++) {
+      habilidades[i].classList.add(habilidadesProgreso[i]);
     }
-
+  }
 }
 
 window.onscroll = function () {
   efectoHabilidades();
-}; 
+};
